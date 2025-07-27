25296f95deb45a00701eaa297786d71f6638c61b
Gustavo Martínez <gustavomartinez1990@gmail.com>
2025-07-27 12:59:26 -0400

    Add tuple example with length calculation and element access

---
+++ platzi-python-basic/data_collection_and_processing/tuple.py
@@ -0,0 +1,11 @@
+# numbers = 1, 2, 3, 4, 5
+numbers = (1, 2, 3, 4, 5)
+print("Tuple:", numbers)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print("Length of tuple:", len(numbers))
+
+# del numbers[0]
